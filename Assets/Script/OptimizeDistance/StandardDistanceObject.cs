dc921824c305cdc258e968788eee8da68008c0b7
amp <hlt315@gmail.com>
2024-09-22 23:05:50 +0900

    Destroy時に破棄

--- Assets/Script/OptimizeDistance/StandardDistanceObject.cs
+++ Assets/Script/OptimizeDistance/StandardDistanceObject.cs
@@ -11,10 +11,11 @@ public class StandardDistanceObject : MonoBehaviour
     private const float DISTANCE = 10f;
     private const float SQR_DISTANCE = DISTANCE * DISTANCE;
 
+    private IDisposable disposable = null;
 
     private void Start()
     {
-        Observable.EveryGameObjectUpdate()
+        disposable = Observable.EveryGameObjectUpdate()
             .Subscribe(_ =>
             {
                 Vector3 cameraPosition = CameraSingleton.Instance.transform.position;
@@ -23,6 +24,11 @@ public class StandardDistanceObject : MonoBehaviour
                 bool isVisible = Vector3Util.SqrDistance(position, cameraPosition) <= SQR_DISTANCE;
 
                 if (gameObject.activeSelf != isVisible) gameObject.SetActive(isVisible);
-            }).AddTo(this);
+            });
+    }
+
+    private void OnDestroy()
+    {
+        disposable.Dispose();
     }
 }
